53d9f7cbcc79f901e32e00a01248551efe8c18c7
markdgold <mgoldstein@utexas.edu>
2021-09-16 13:23:40 -0500

    remove starter code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import * as angular from 'angular';
  * Import Application Modules
  */
 import { moduleName as coreModule } from './core/core.module';
-import { moduleName as contactsModule } from './contacts/contacts.module';
 import { moduleName as marketModule } from './market/market.module';
 
 export const moduleName =
   angular.module('application', [
     coreModule,
-    contactsModule,
     marketModule
   ])
   .name;
